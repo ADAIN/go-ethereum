dc8882868a99fc697972356ec35543cf5eaf5a3e
SungYong Jang <jsy@adain.kr>
2017-09-18 03:08:00 +0900

    add bootnodes

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./build/bin/geth --networkid 20170923 --rpc --mine --datadir eth-data console
+./build/bin/geth --networkid 20170923 --rpc --mine --datadir eth-data console --bootnodes enode://af1c8717da5db8b478a58dc53be4823cf5b611729a066f8fea4acdb450dbdd232c201a726e168bf412b99894c98715bed85450a5882c367670955d0b886f11c2@220.73.128.181:30303
