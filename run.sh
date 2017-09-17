031a9d1651bac4f6a4c04d38b3c2afc15c0ccc0e
SungYong Jang <jsy@adain.kr>
2017-09-18 02:06:13 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./build/bin/geth --networkid 20170923 --rpc --datadir eth-data console --bootnodes enode://63b528b88885073a43c73f512f62bff61a7b2d5acdfc54a73ec1ab564e38be4813d9c12365a6c8bf2ac1321162ac6c2ba7e73d5b9803a7129eb5ad5d60e61bc6@220.73.128.181:30303
+./build/bin/geth --networkid 20170923 --rpc --mine --datadir eth-data console --bootnodes enode://63b528b88885073a43c73f512f62bff61a7b2d5acdfc54a73ec1ab564e38be4813d9c12365a6c8bf2ac1321162ac6c2ba7e73d5b9803a7129eb5ad5d60e61bc6@220.73.128.181:30303
